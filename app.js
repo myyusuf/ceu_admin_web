6b44351c6b56878c9510dc528116a48921cb5658
myyusuf <myyusuf1911@gmail.com>
2017-04-06 15:07:39 +0700

    Ant design

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './Menu.jsx';
+
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+
+import DatePicker from 'antd/lib/date-picker';
 
 ReactDOM.render(
-  <Menu />,
+  <LocaleProvider locale={enUS}><DatePicker /></LocaleProvider>,
   document.getElementById('app')
 );
