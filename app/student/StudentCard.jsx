99f1c9bb6a8ad1aa6ac1666eb202a601aa639ba4
myyusuf <myyusuf1911@gmail.com>
2017-04-06 16:29:51 +0700

    component test

--- app/student/StudentCard.jsx
+++ app/student/StudentCard.jsx
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import Card from 'antd/lib/card';
+import Tag from 'antd/lib/tag';
 
 export default class StudentCard extends Component {
 
   constructor(){
     super();
     this.state = {
-      title: 'Hello'
+      name: 'Hello',
+      status: 'Active'
     }
   }
   render() {
+
+    var statusTag = <Tag color="#2db7f5">{this.state.status}</Tag>;
     return (
-      <Card title={this.state.title} extra={<a href="#">More</a>} style={{ width: 300 }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+      <Card style={{ width: 300 }}>
+        <table style={{ width: 300 }}>
+          <tbody>
+            <tr>
+              <td style={{ width: 150 }}>
+                <img src="assets/images/avatar.png"></img>
+              </td>
+              <td style={{ width: 150 }}>
+
+              </td>
+            </tr>
+          <tr>
+            <td style={{ width: 150 }}>
+              Status
+            </td>
+            <td style={{ width: 150 }}>
+              <Tag color="#2db7f5">{this.state.status}</Tag>
+            </td>
+          </tr>
+          </tbody>
+        </table>
       </Card>
     );
   }
