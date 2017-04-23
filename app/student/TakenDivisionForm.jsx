d69ca288458731c25803b56bebb1db009b9c6d65
myyusuf <myyusuf1911@gmail.com>
2017-04-23 18:21:04 +0700

    Taken division form

--- app/student/TakenDivisionForm.jsx
+++ app/student/TakenDivisionForm.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Form from 'antd/lib/form';
+import Tooltip from 'antd/lib/tooltip';
 import Icon from 'antd/lib/icon';
 import Input from 'antd/lib/input';
 import Checkbox from 'antd/lib/checkbox';
 import Button from 'antd/lib/button';
 import axios from 'axios';
+import Col from 'antd/lib/col';
 
 const FormItem = Form.Item;
 
@@ -14,6 +16,15 @@ export default class TakenDivisionForm extends Component {
     super(props);
     this.state = {
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+  }
+
+  handlePick() {
+    this.props.form.setFieldsValue({
+      nama: 'Hi..',
+    });
   }
 
   handleSubmit(e) {
@@ -27,36 +38,90 @@ export default class TakenDivisionForm extends Component {
 
   render() {
     const { getFieldDecorator } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 14 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 14,
+          offset: 6,
+        },
+      },
+    };
+
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
-              type="password" placeholder="Password" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </FormItem>
+        <FormItem
+         {...formItemLayout}
+           label={(
+             <span>
+               Nama
+             </span>
+           )}
+           value="ss"
+         hasFeedback>
+           {getFieldDecorator('nama', {
+             rules: [{ required: true, message: 'Isi dengan nama lengkap!', whitespace: true }],
+           })(
+             <Input />
+           )}
+         </FormItem>
+
+         <FormItem
+          {...formItemLayout}
+            label={(
+              <span>
+                Stambuk Lama&nbsp;
+                <Tooltip title="Stambuk lama mahasiswa">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>
+            )}
+          hasFeedback>
+            {getFieldDecorator('stambukLama', {
+              rules: [{ required: true, message: 'Isi dengan nama stambuk lama!', whitespace: true }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+
+          <FormItem
+           {...formItemLayout}
+             label={(
+               <span>
+                 Stambuk Baru&nbsp;
+                 <Tooltip title="Stambuk baru mahasiswa">
+                   <Icon type="question-circle-o" />
+                 </Tooltip>
+               </span>
+             )}
+           hasFeedback>
+             {getFieldDecorator('stambukBaru', {
+               rules: [{ required: true, message: 'Isi dengan nama stambuk baru!', whitespace: true }],
+             })(
+               <div>
+                 <Col span={18}><Input /></Col>
+                 <Col span={6}><Button type="dashed" size="large" onClick={this.handlePick} style={{ marginLeft: 10 }}>Select</Button></Col>
+                 </div>,
+             )}
+           </FormItem>
+
+         <FormItem {...tailFormItemLayout}>
+           <Button type="primary" htmlType="submit" size="large">Save</Button>
+         </FormItem>
       </Form>
     );
   }
