53936af1b6115af9c8c5f518b838d9c73cb93724
myyusuf <myyusuf1911@gmail.com>
2017-04-29 22:39:40 +0700

    student card list

--- app/student/StudentCardList.jsx
+++ app/student/StudentCardList.jsx
@@ -3,10 +3,13 @@ import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
 import Button from 'antd/lib/button';
 import Radio from 'antd/lib/radio';
+import Select from 'antd/lib/select';
 import Input from 'antd/lib/input';
 import Pagination from 'antd/lib/pagination';
 import axios from 'axios';
 import StudentCard from './StudentCard';
+
+const Option = Select.Option;
 
 export default class StudentCardList extends Component {
 
@@ -14,7 +17,7 @@ export default class StudentCardList extends Component {
     super(props);
     this.state = {
       students: [],
-      searchFilter: 'aktif',
+      searchFilter: '1',
     };
 
     this.showDetails = this.showDetails.bind(this);
@@ -53,6 +56,10 @@ export default class StudentCardList extends Component {
     this.setState({ searchFilter: e.target.value });
   }
 
+  handleChange(value) {
+    console.log(value);
+  }
+
   render() {
     const cardList = [];
     const colsPerRow = 3;
@@ -86,21 +93,47 @@ export default class StudentCardList extends Component {
 
     const searchFilter = this.state.searchFilter;
 
+    const statusArray = [];
+    statusArray.push(<Option key="1">Aktif</Option>);
+    statusArray.push(<Option key="2">Bermasalah</Option>);
+    statusArray.push(<Option key="3">Lulus</Option>);
+
     return (
       <div className="student-card-list">
         <div className="card-list-header">
-          <Input
-            className="search-text"
-            placeholder="Nama atau Stambuk"
-          />
-          <Button shape="circle" icon="search" className="search-button" />
-          <Button shape="circle" type="primary" icon="download" className="download-button" />
-          <div className="filter-button">
-            <Radio.Group value={searchFilter} onChange={this.handleSizeChange}>
-              <Radio.Button value="aktif" icon="plus">Aktif</Radio.Button>
-              <Radio.Button value="bermasalah">Bermasalah</Radio.Button>
-              <Radio.Button value="lulus">Lulus</Radio.Button>
-            </Radio.Group>
+          <div className="search">
+            <ul>
+              <li>
+                <Input
+                  style={{ width: 200 }}
+                  className="search-text"
+                  placeholder="Nama atau Stambuk"
+                />
+              </li>
+              <li>
+                <Select
+                  mode="multiple"
+                  style={{ minWidth: 100 }}
+                  placeholder="Status"
+                  defaultValue={['1']}
+                  onChange={this.handleChange}
+                >
+                  {statusArray}
+                </Select>
+              </li>
+              <li>
+                <Radio.Group value={searchFilter} onChange={this.handleSizeChange}>
+                  <Radio.Button value="1" icon="plus"> 1 </Radio.Button>
+                  <Radio.Button value="2"> 2 </Radio.Button>
+                </Radio.Group>
+              </li>
+              <li>
+                <Button shape="circle" icon="search" className="search-button" />
+              </li>
+              <li>
+                <Button shape="circle" type="primary" icon="download" />
+              </li>
+            </ul>
           </div>
           <div className="pagination">
             <Pagination simple defaultCurrent={1} total={50} />
