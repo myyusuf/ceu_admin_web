36bb229f27663ae9d2d1049d7603c78bfeb7bed1
myyusuf <myyusuf1911@gmail.com>
2017-04-29 12:48:19 +0700

    new workspace CSS

--- app/workspace/Workspace.jsx
+++ app/workspace/Workspace.jsx
@@ -1,16 +1,26 @@
 
 import React from 'react';
+import SideMenu from './SideMenu';
+import LoginInfo from '../user/LoginInfo';
 
 const Workspace = () => {
   return (
-    <div className="workspace-header">
-      <img className="logo" src="assets/images/small_logo.png" alt="Logo" />
-      <span className="app-name">CEU</span>
-      <nav>
-        <ul>
-          <li><img src="assets/images/icons/settings.png" alt="menu" /></li>
-        </ul>
-      </nav>
+    <div>
+      <div className="workspace-header">
+        <img className="logo" src="assets/images/small_logo.png" alt="Logo" />
+        <span className="app-name">CEU</span>
+        <nav>
+          <ul>
+            <li><img src="assets/images/icons/settings.png" alt="menu" /></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="workspace-left-menu">
+        <LoginInfo name="Yusuf" roleName="Administrator" />
+        <SideMenu />
+      </div>
+      <div className="workspace-section">
+      </div>
     </div>
   );
 };
