f62ddd4810ee40d45f8f5d0c12a9475092395e1e
myyusuf <myyusuf1911@gmail.com>
2017-05-27 07:07:53 +0700

    Add taken department

--- app/student/taken_department/AddTakenDepartmentForm.jsx
+++ app/student/taken_department/AddTakenDepartmentForm.jsx
@@ -3,10 +3,13 @@ import Form from 'antd/lib/form';
 import Input from 'antd/lib/input';
 import Select from 'antd/lib/select';
 import Modal from 'antd/lib/modal';
+import Message from 'antd/lib/message';
 import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
+import axios from 'axios';
 
 const Option = Select.Option;
+const OptGroup = Select.OptGroup;
 const FormItem = Form.Item;
 
 export default class AddTakenDepartmentForm extends Component {
@@ -14,6 +17,13 @@ export default class AddTakenDepartmentForm extends Component {
   constructor(props) {
     super(props);
     this.onColorChange = this.onColorChange.bind(this);
+    this.state = {
+      departments: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getDepartments();
   }
 
   onColorChange(color) {
@@ -22,28 +32,71 @@ export default class AddTakenDepartmentForm extends Component {
     });
   }
 
+  getDepartments() {
+    this.setState({ loading: true }, () => {
+      axios.get('/departments_all', {
+        params: {
+          searchText: this.state.searchText,
+          studentLevel: this.state.studentLevel,
+        },
+      })
+      .then((response) => {
+        this.setState({
+          departments: response.data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.dir(error);
+        let errorMessage = 'Error occured when doing server request.\n';
+        if (error.message) {
+          errorMessage += error.message;
+        }
+        Message.error(errorMessage);
+      });
+    });
+  }
+
   render() {
     const { visible, onCancel, onCreate, form } = this.props;
     const { getFieldDecorator } = form;
+
+    const level1Departments = this.state.departments.filter((department) => {
+      return department.tingkat === 1;
+    });
+    const level1Options = level1Departments.map((department) => {
+      return <Option key={department.kode}>{department.nama}</Option>;
+    });
+
+    const level2Departments = this.state.departments.filter((department) => {
+      return department.tingkat === 2;
+    });
+    const level2Options = level2Departments.map((department) => {
+      return <Option key={department.kode}>{department.nama}</Option>;
+    });
+
     return (
       <Modal
         visible={visible}
         wrapClassName="vertical-center-modal"
         width={450}
-        title="Bagian Baru"
+        title="Tambah Bagian"
         okText="Create"
         onCancel={onCancel}
         onOk={onCreate}
       >
         <Form layout="vertical">
           <Row gutter={15}>
-            <Col span={8}>
-              <FormItem label="Tingkat">
-                {getFieldDecorator('tingkat', {
+            <Col span={10}>
+              <FormItem label="Bagian">
+                {getFieldDecorator('bagian', {
                   rules: [
                     {
                       required: true,
-                      message: 'Tingkat wajib diisi',
+                      message: 'Bagian wajib diisi',
                     },
                   ],
                 })(
@@ -52,13 +105,17 @@ export default class AddTakenDepartmentForm extends Component {
                     placeholder="Pilih bagian"
                     style={{ width: '100%' }}
                   >
-                    <Option key="1">Tingkat 1</Option>
-                    <Option key="2">Tingkat 2</Option>
+                    <OptGroup label="Tingkat 1">
+                      {level1Options}
+                    </OptGroup>
+                    <OptGroup label="Tingkat 2">
+                      {level2Options}
+                    </OptGroup>
                   </Select>,
                 )}
               </FormItem>
             </Col>
-            <Col span={16}>
+            <Col span={14}>
               <FormItem label="Nama">
                 {getFieldDecorator('nama', {
                   rules: [
